90ef25f2cb9224199dc97666d0fe0f22f3c5cd1b
Wei Su <suwei779@gmail.com>
2025-05-04 21:23:54 -0700

    Change the demo to randomly sampling X songs and outputing json dataset with lyrics populated

--- lyrics.py
+++ lyrics.py
@@ -7,7 +7,7 @@ import json
 from typing import Optional
 
 
-def search(title: str, artist: str) -> list:
+def search_from_lrclib(title: str, artist: str) -> list:
     params = {
         "q": "",
         "track_name": title,
@@ -40,6 +40,52 @@ def extract_artist(song: dict):
     return name
 
 
+def search(song: dict):
+    search_title = extract_title(song)
+    search_artist = extract_artist(song)
+    res = {
+        "search_title": search_title,
+        "search_artist": search_artist,
+        "found": False
+    }
+
+    lyrics_list = search_from_lrclib(search_title, search_artist)
+    if len(lyrics_list) > 0:
+        lyrics = lyrics_list[0]
+        res.update({
+            "found": True,
+            "result_title": lyrics["trackName"],
+            "result_artist": lyrics["artistName"],
+            "result_album": lyrics["albumName"],
+            "duration": lyrics["duration"],
+            "lyrics": lyrics["plainLyrics"],
+        })
+    return res
+
+
+def sample(song_list: list[dict], num_samples: int = 20, outfile: str = "samples.json", verbose: bool = False):
+    res = []
+    N = len(song_list)
+    while len(res) < num_samples:
+        i = random.randint(0, N)
+        lyrics = search(song_list[i])
+        if lyrics["found"] and lyrics["lyrics"] is not None:
+            if verbose:
+                print("="*80)
+                print("Title: " + lyrics["result_title"])
+                print("Artist Name: " + lyrics["result_artist"])
+                print("Album Name: " + lyrics["result_album"])
+                print(f"Duration: {lyrics['duration']}s")
+                print("Lyrics: ")
+                print(lyrics["lyrics"])
+
+            #lyrics["lyrics"] = lyrics["lyrics"].encode("raw_unicode_escape").decode("utf-8")
+            res.append(lyrics)
+    with open(outfile, "w") as f:
+        json.dump(res, f, indent=2, ensure_ascii=False)
+    return len(res)
+
+
 def demo():
     if len(sys.argv) < 2:
         print("Please specify a music history json file.")
@@ -51,24 +97,31 @@ def demo():
     N = len(history)
     song = history[random.randint(0, N)]
 
-    title = extract_title(song)
-    artist = extract_artist(song)
+    lyrics = search(song)
 
-    print("Title: " + title)
-    print("Artist: " + artist)
+    print(f"Search criteria: {lyrics['search_title']}, {lyrics['search_artist']}")
 
-    lyrics_list = search(title, artist)
-
-    for lyrics in lyrics_list:
+    if lyrics["found"]:
         print("="*80)
-        print("Track Name: " + lyrics["trackName"])
-        print("Artist Name: " + lyrics["artistName"])
-        print("Album Name: " + lyrics["albumName"])
+        print("Title: " + lyrics["result_title"])
+        print("Artist Name: " + lyrics["result_artist"])
+        print("Album Name: " + lyrics["result_album"])
         print(f"Duration: {lyrics['duration']}s")
         print("Lyrics: ")
-        print(lyrics["plainLyrics"])
+        print(lyrics["lyrics"])
 
+
+def demo2():
+    if len(sys.argv) < 2:
+        print("Please specify a music history json file.")
+        exit(1)
+
+    with open(sys.argv[1], "r") as f:
+        history = json.load(f)
+
+
+    sample(history, verbose=True)
 
 
 if __name__ == "__main__":
-    demo()
+    demo2()
