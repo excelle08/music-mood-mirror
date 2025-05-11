e1edca946c0ac5aafcc6b95a7d304a6b06b9d8ea
Wei Su <suwei779@gmail.com>
2025-05-10 19:17:07 -0700

    Refactor lyrics.py, support both YTB music and spotify history

--- lyrics.py
+++ lyrics.py
@@ -5,62 +5,148 @@ import sys
 import random
 import json
 from typing import Optional
+from datetime import datetime
 
 
-def search_from_lrclib(title: str, artist: str) -> list:
-    params = {
-        "q": "",
-        "track_name": title,
-        "artist_name": artist,
-    }
-    req = requests.get("https://lrclib.net/api/search", params=params)
-    return json.loads(req.text)
+class Song:
+    """
+    A class representing a song with its metadata and lyrics.
+    """
+    def __init__(self, title: str, artist: str, album: Optional[str] = None, play_datetime: Optional[str] = None):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.lyrics = None
+        # If play_datetime is supplied, convert it to a datetime object
+        # The datetime should be in ISO format
+        if play_datetime is not None:
+            try:
+                self.play_datetime = datetime.fromisoformat(play_datetime)
+            except ValueError:
+                print(f"Invalid datetime format: {play_datetime}")
+                self.play_datetime = None
+        self.result_title = None
+        self.result_artist = None
+        self.result_album = None
+        self.duration = None
+        self.synced_lyrics = None
+
+    def __str__(self):
+        return f"Title: {self.title}, Artist: {self.artist}, Album: {self.album}"
+
+    def __repr__(self):
+        return f"Song(title={self.title}, artist={self.artist}, album={self.album})"
+
+    def to_dict(self):
+        """
+        Convert the Song object to a dictionary.
+        :return: A dictionary representation of the Song object.
+        """
+        return {
+            "title": self.title,
+            "artist": self.artist,
+            "album": self.album,
+            "play_datetime": self.play_datetime.strftime('%Y-%m-%d %H:%M:%S') if self.play_datetime else None,
+            "result_title": self.result_title,
+            "result_artist": self.result_artist,
+            "result_album": self.result_album,
+            "duration": self.duration,
+            "lyrics": self.lyrics,
+            "synced_lyrics": self.synced_lyrics
+        }
+
+    def search_lyrics(self) -> dict:
+        """
+        Search for lyrics using the lrclib API.
+        :param title: The title of the song.
+        :param artist: The artist of the song.
+        :return: A list of lyrics dictionaries.
+        """
+        params = {
+            "q": "",
+            "track_name": self.title,
+            "artist_name": self.artist,
+        }
+        if self.album is not None:
+            params["album_name"] = self.album
+        req = requests.get("https://lrclib.net/api/search", params=params, timeout=10)
+        search_results = json.loads(req.text)
+        if len(search_results) == 0:
+            return {}
+        res = search_results[0]
+        self.result_title = res["trackName"]
+        self.result_artist = res["artistName"]
+        self.result_album = res["albumName"]
+        self.duration = res["duration"]
+        self.lyrics = res["plainLyrics"]
+        self.synced_lyrics = res["syncedLyrics"]
+        return res
+
+    @classmethod
+    def from_youtube(cls, song: dict):
+        """
+        Create a Song object from a YouTube music listening history entry.
+        :param song: A dictionary representing a YouTube music history entry.
+        :return: A Song object if creation is successful, or None.
+        """
+        # Check validity: "header" is "Youtube Music"
+        if "header" not in song or song["header"] != "YouTube Music":
+            return None
+        # Extract title
+        title = song["title"]
+        if "Watched" in title:
+            title = title[8:]
+        # Extract artist: the field "subtitles" should exist, is a list of dicts,
+        # has at least one entry, the first entry has a field "name" which is the artist name.
+        # It should end with "Topic" and we should remove it.
+        if "subtitles" not in song or len(song["subtitles"]) < 1:
+            return None
+        subtitles = song["subtitles"]
+        if len(subtitles) < 1:
+            return None
+        name = subtitles[0]["name"]
+        if name.endswith("Topic"):
+            name = name[:len(name)-8].strip()
+        else:
+            return None
+        # Create the Song object
+        return cls(title=title, artist=name, play_datetime=song["time"])
+
+    @classmethod
+    def from_spotify(cls, song: dict):
+        """
+        Create a Song object from a Spotify music listening history entry.
+        :param song: A dictionary representing a Spotify music history entry.
+        :return: A Song object if creation is successful, or None.
+        """
+        # Check validity: the field "spotify_track_uri" should exist
+        if "spotify_track_uri" not in song:
+            return None
+        # Extract title, artist and album
+        title = song["master_metadata_track_name"]
+        artist = song["master_metadata_album_artist_name"]
+        album = song["master_metadata_album_album_name"]
+        # Create the Song object
+        return cls(title=title, artist=artist, album=album, play_datetime=song["ts"])
 
 
-def extract_title(song: dict):
-    # Strip "Watched" prefix
-    orig_title = song["title"]
-    if orig_title.startswith("Watched"):
-        title = orig_title[8:]
+def search(song: dict) -> Song:
+    """
+    Search for lyrics using the lrclib API.
+    :param song: A dictionary representing a youtube or spotify listening history entry.
+    :return: A Song object if creation is successful, or None.
+    """
+    if "header" in song and song["header"] == "YouTube Music":
+        s = Song.from_youtube(song)
+    elif "spotify_track_uri" in song:
+        s = Song.from_spotify(song)
     else:
-        title = orig_title
+        s = None
 
-    return title.strip()
+    if s is not None:
+        s.search_lyrics()
 
-
-def extract_artist(song: dict):
-    if "subtitles" not in song:
-        return ""
-    subtitles = song["subtitles"]
-    if len(subtitles) < 1:
-        return ""
-    name = subtitles[0]["name"]
-    if name.endswith("Topic"):
-        return name[:len(name)-8].strip()
-    return name
-
-
-def search(song: dict):
-    search_title = extract_title(song)
-    search_artist = extract_artist(song)
-    res = {
-        "search_title": search_title,
-        "search_artist": search_artist,
-        "found": False
-    }
-
-    lyrics_list = search_from_lrclib(search_title, search_artist)
-    if len(lyrics_list) > 0:
-        lyrics = lyrics_list[0]
-        res.update({
-            "found": True,
-            "result_title": lyrics["trackName"],
-            "result_artist": lyrics["artistName"],
-            "result_album": lyrics["albumName"],
-            "duration": lyrics["duration"],
-            "lyrics": lyrics["plainLyrics"],
-        })
-    return res
+    return s
 
 
 def sample(song_list: list[dict], num_samples: int = 20, outfile: str = "samples.json", verbose: bool = False):
@@ -68,20 +154,22 @@ def sample(song_list: list[dict], num_samples: int = 20, outfile: str = "samples
     N = len(song_list)
     while len(res) < num_samples:
         i = random.randint(0, N)
-        lyrics = search(song_list[i])
-        if lyrics["found"] and lyrics["lyrics"] is not None:
+        print(i)
+        song = search(song_list[i])
+        if song is not None and song.lyrics is not None:
             if verbose:
                 print("="*80)
-                print("Title: " + lyrics["result_title"])
-                print("Artist Name: " + lyrics["result_artist"])
-                print("Album Name: " + lyrics["result_album"])
-                print(f"Duration: {lyrics['duration']}s")
+                print("Title: " + song.result_title)
+                print("Artist Name: " + song.result_artist)
+                print("Album Name: " + song.result_album)
+                print(f"Duration: {song.duration}s")
                 print("Lyrics: ")
-                print(lyrics["lyrics"])
+                print(song.lyrics)
 
             #lyrics["lyrics"] = lyrics["lyrics"].encode("raw_unicode_escape").decode("utf-8")
-            res.append(lyrics)
-    with open(outfile, "w") as f:
+            res.append(song.to_dict())
+
+    with open(outfile, "w", encoding="utf-8") as f:
         json.dump(res, f, indent=2, ensure_ascii=False)
     return len(res)
 
@@ -91,7 +179,7 @@ def demo():
         print("Please specify a music history json file.")
         exit(1)
 
-    with open(sys.argv[1], "r") as f:
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
         history = json.load(f)
 
     N = len(history)
@@ -116,9 +204,8 @@ def demo2():
         print("Please specify a music history json file.")
         exit(1)
 
-    with open(sys.argv[1], "r") as f:
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
         history = json.load(f)
-
 
     sample(history, verbose=True)
 
