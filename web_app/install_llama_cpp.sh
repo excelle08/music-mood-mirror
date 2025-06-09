5dab151ffefe29193032e427ae0edbdc16a3f637
Wei Su <suwei779@gmail.com>
2025-06-08 21:11:11 -0700

    Add the script to install llama-cpp-python

---
+++ web_app/install_llama_cpp.sh
@@ -0,0 +1,5 @@
+CMAKE_ARGS="-DGGML_CUDA=on"
+pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir --no-binary llama-cpp-python
+pip install huggingface-hub
+pip install hf_transfer
+
